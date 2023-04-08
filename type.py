298f8c3ea29d9fe2c4598228ddd53899286da2cc
Isuru Fernando <69768677+IsuruFerna@users.noreply.github.com>
2023-04-08 08:06:30 +0200

    types

---
+++ type.py
@@ -0,0 +1,8 @@
+## this will show you the type of "50". In this case "50" is a int and return <class 'int'>
+print(type(50))
+
+print(type("Hello, world"))
+print(type([]))
+print(type(list()))
+print(type({}))
+print(type(dict()))
