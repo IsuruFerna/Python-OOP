043089eacdf7b289c2ca30fea87a84688bba685a
Isuru Fernando <69768677+IsuruFerna@users.noreply.github.com>
2023-04-08 08:05:45 +0200

    getter and setter, getter == @property

--- student.py
+++ student.py
@@ -87,29 +87,72 @@
 
 ## Case 6.1 - Using classes and functions inside of them
 class Student:
-  def __init__(self, name, house, patronus):
-    if not name:
-      raise ValueError("Missing name")
-    if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-      raise ValueError("Invalid house")
+  # def __init__(self, name, house, patronus):
+  def __init__(self, name, house):
+    # if not name:
+    #   raise ValueError("Missing name")
+    # if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+    #   raise ValueError("Invalid house")
     self.name = name
     self.house = house
-    self.patronus = patronus
+    # self.patronus = patronus
 
   def __str__(self):
     return f"{self.name} from {self.house}"
   ## you can use __repr__ to get more informations and details about things that inside of the object. like what type of objects
 
+  # Getter
+  @property
+  def house(self):
+    return self._house
+
+  # Setter  
+  @house.setter
+  def house(self, house):
+    if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+      raise ValueError("Invlid house")
+    self._house = house
+
+
+  @property
+  def name(self):
+    return self._name
+  
+  @name.setter
+  def name(self, name):
+    if not name:
+      raise ValueError("Missing name")
+    self._name = name
+
+
+  # def charm(self):
+  #   match self.patronus:
+  #     case "Stag":
+  #       return "🐴"
+  #     case "Otter":
+  #       return "🦦"
+  #     case "Jack Russel terrieer":
+  #       return "🐶"
+  #     case _:
+  #       return "/"
+
 def main():
   student = get_student()
+
+  # ## This will blocked by setter and raise ValueError
+  # student.house = "SSC"
+
   # print(f"{student.name} from {student.house}")
   print(student)
+  # print("Expecto Patronum!")
+  # print(student.charm())
 
 ### method 2
 def get_student():
   name = input("Name: ")
   house = input("House: ")
-  patronus = input("Patronus: ")
+  # patronus = input("Patronus: ")
+  # return Student(name, house, patronus)
   return Student(name, house)
     
 
